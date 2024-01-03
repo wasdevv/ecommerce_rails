2cd9a0ad0c34359c6caeeb22ebec0b0a2a950d2a
Washington <cardosowashington2003@gmail.com>
2024-01-03 17:32:40 -0300

    2 commit - configs in views,controller and generate the devise user

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,25 @@
 class ProductsController < ApplicationController
+    def index
+        @products = Product.all
+    end
+
+    def new
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+    
+    private
+
+    def product_params
+    end
 end
