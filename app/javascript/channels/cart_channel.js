6413445f07dcbf9f346e6a42495f51dab9c5e983
Washington <cardosowashington2003@gmail.com>
2024-01-15 22:19:28 -0300

    21 commit - finishing the branch for this moment 15/01

--- app/javascript/channels/cart_channel.js
+++ app/javascript/channels/cart_channel.js
@@ -13,7 +13,7 @@ consumer.subscriptions.create({ channel: "CartChannel" }, {
     console.log("Received", data);
 
     if (data.action === 'update_cart') {
-      console.log(`Cart ID: ${data.cart.id}, Number of Products: ${data.cart.cart_items.length}`);
+      console.log(`Cart ID: ${data.cart.id}, Number of Products: ${data.cart.cart_items ? data.cart.cart_items.length : 0}`);
       TurboStreams.append("cart", { partial: "cart/cart", locals: { cart: data.cart } });
     } else if (data.action === 'product_removed') {
       console.log(`Product removed from cart - Product ID: ${data.product_id}`);
