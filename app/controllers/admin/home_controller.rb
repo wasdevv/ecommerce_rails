c153380eab30b13b0d567f674dfba36d5e8a9e49
Washington <contatxpriv@gmail.com>
2024-03-04 20:33:18 -0300

    Admin Dashboard in navbar, now trying to put all logs on admindashboard

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -1,16 +1,6 @@
 class Admin::HomeController < AdminController
     before_action :authenticate_user!
-    before_action :check_role # Podendo ser modificado para outros roles dentro do projeto
 
     def index
-    end
-
-    private
-
-    def check_role
-        unless current_user.role == 1 # Caso o usuário NÃO seja a role 1(Administrador)
-            flash[:alert] = 'Você não tem permissão para acessar essa página. '
-            redirect_to root_path
-        end
     end
 end
